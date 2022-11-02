0a429f6a140b06d88c8d38680053aeeaac304555
Sanjay <scshitole@gmail.com>
2022-11-02 13:36:05 -0700

    added f5

--- vpc.tf
+++ vpc.tf
@@ -97,5 +97,43 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_eip" "f5" {
+  instance = aws_instance.f5.id
+  vpc      = true
+}
+
+
+resource "aws_security_group" "f5" {
+  name   = "${var.prefix}-f5"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
  
